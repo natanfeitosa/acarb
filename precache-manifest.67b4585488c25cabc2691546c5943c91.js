e411969bc5fc90f778d89a66f11b7f0e4ff4bbd9
Natanael dos Santos Feitosa <natansantosapps@gmail.com>
2021-09-02 09:53:09 -0300

    GTag added

---
+++ precache-manifest.67b4585488c25cabc2691546c5943c91.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76d610251be9a034fde2ba9b7e9c03e5",
+    "url": "browserconfig.xml"
+  },
+  {
+    "revision": "7e254785cd0a27a3e1e0",
+    "url": "css/app.d00e680c.css"
+  },
+  {
+    "revision": "2c63dc921a27440e070c6f7c2305b392",
+    "url": "img/poluicao_ambiental.2c63dc92.jpg"
+  },
+  {
+    "revision": "13785edbc5315e9f432492cc71410c28",
+    "url": "img/reciclando.13785edb.png"
+  },
+  {
+    "revision": "fc44064f565dbba4be1b7aeadcde611d",
+    "url": "img/rio_poluido.fc44064f.png"
+  },
+  {
+    "revision": "35fdb6766345d833e6e14ba1d982584a",
+    "url": "index.html"
+  },
+  {
+    "revision": "7e254785cd0a27a3e1e0",
+    "url": "js/app.4eef793b.js"
+  },
+  {
+    "revision": "7c11d59e7eadaf1fbbe3",
+    "url": "js/chunk-vendors.ab0c83e4.js"
+  },
+  {
+    "revision": "c1d60df692f18773c489",
+    "url": "js/sobre.36b4b97c.js"
+  },
+  {
+    "revision": "ed5b3fe4c2191fc2d7d95dba97546a8f",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+]);
