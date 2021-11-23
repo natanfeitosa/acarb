ac2b4d158d7dad7b4c2b19fd4f8a99d6456e181e
Natanael dos Santos Feitosa <52074821+natanfeitosa@users.noreply.github.com>
2021-11-23 19:34:38 +0000

    css updates dev

--- src/App.vue
+++ src/App.vue
@@ -78,7 +78,7 @@
         </b-row>
       </div>
     </footer>
-    <b-button v-if="showBToTop" @click="totop" variant="light" class="border rounded-circle" style="z-index: 1030; position: fixed; right: 26px; bottom: 28px; padding: 10px 12px; box-shadow: 0 0 20px 10px rgb(0 0 0 / 12%);">
+    <b-button v-if="showBToTop" @click="totop" variant="light" class="border rounded-circle" style="z-index: 1030; position: fixed; right: 12px; bottom: 10px; padding: 10px 12px; box-shadow: 0 0 20px 10px rgb(0 0 0 / 12%);">
       <svg aria-hidden="true" focusable="false" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" width="20px">
         <path fill="currentColor" d="M240.971 130.524l194.343 194.343c9.373 9.373 9.373 24.569 0 33.941l-22.667 22.667c-9.357 9.357-24.522 9.375-33.901.04L224 227.495 69.255 381.516c-9.379 9.335-24.544 9.317-33.901-.04l-22.667-22.667c-9.373-9.373-9.373-24.569 0-33.941L207.03 130.525c9.372-9.373 24.568-9.373 33.941-.001z"></path>
       </svg>
